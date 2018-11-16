37ff53fd745b17b128cbe34ec4aa3487189a2f90
zhoukai <189898933@qq.com>
2018-11-16 11:14:24 +0800

    提供时间转换工具类（可继承改类继续扩展）

---
+++ util/src/main/java/com/tanker/base/util/DateUtils.java
@@ -0,0 +1,46 @@
+package com.tanker.base.util;
+
+import android.annotation.SuppressLint;
+
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author : Tanker
+ * @email :zhoukai@zto.cn
+ * @date : 2018/11/16
+ * @describe : 时间工具类
+ */
+public class DateUtils {
+
+    public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+    public static final String YYYY_MM_DD = "yyyy-MM-dd";
+
+    public static String dataToStr(Date date){
+        return getDateFormat(YYYY_MM_DD_HH_MM_SS).format(date);
+    }
+
+    public static Date strToData(String date){
+        return getDateFormat(YYYY_MM_DD).parse(date,new ParsePosition(0));
+    }
+
+    /**
+     * 获取当前时间
+     *
+     * @return
+     */
+    protected static Date getTimeAsDate() {
+        return new Date();
+    }
+
+    protected static long getNowTimeAslong() {
+        return System.currentTimeMillis();
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    protected static SimpleDateFormat getDateFormat(String timePattern){
+        return new SimpleDateFormat(timePattern);
+    }
+
+}
